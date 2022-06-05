375162469ba53d348c2717da05a11b5a7699b52b
Ben <benjanac.dev@gmail.com>
2022-06-04 22:21:53 -0400

    fixed bug for [0,0] space, clarified check variables

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-
-
 alert('hello world');
+
+// things we need:
+// - event listeners to check for clicks which runs an update function
+// - an
 
 let grid = [[0,0,0],[0,0,0],[0,0,0]]
 let key = {'XXX': 'p1', 'OOO': 'p2'}
@@ -18,34 +20,38 @@ function checkGrid(grid) {
                 }
             }
 
-            let line = ''
-
+            let vertical = ''
             if (i == 0) {
                 for (let q = 0; q < grid.length; q++) {
-                    line += grid[q][j]
+                    vertical += grid[q][j]
                 }
-            } else if (j == 0) {
+            }
+            if (key[vertical] != undefined) {
+                return key[vertical]
+            }
+            
+            let horizontal = ''
+            if (j == 0) {
                 for (let q = 0; q < grid[i].length; q++) {
-                    line += grid[i][q]
+                    horizontal += grid[i][q]
                 }
             }
-
-            if (key[line] != undefined) {
-                return key[line]
+            if (key[horizontal] != undefined) {
+                return key[horizontal]
             }
 
+            let diagonal = ''
             if (i == 0 && j == 0) {
                 for (let q = 0; q < grid.length; q++) {
-                    line += grid[i+q][j+q]
+                    diagonal += grid[i+q][j+q]
                 }
             } else if (i == 0 && j == 2) {
                 for (let q = 0; q < grid.length; q++) {
-                    line += grid[i+q][j-q]
+                    diagonal += grid[i+q][j-q]
                 }
             }
-
-            if (key[line] != undefined) {
-                return key[line]
+            if (key[diagonal] != undefined) {
+                return key[diagonal]
             }
 
         }
