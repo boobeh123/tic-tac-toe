50fd3f613cf0afccd50425bc7d143535f9ed600c
Bobby Asakawa <101483481+boobeh123@users.noreply.github.com>
2022-06-25 15:53:45 -1000

    Revert "added write functionality and switching between players"

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 class TicTacToe {
     constructor() {
         // Properties
-        this.spaces = document.querySelectorAll('.box')
         this.grid = [[0,0,0],[0,0,0],[0,0,0]];
         this.key = {'XXX': 'p1', 'OOO': 'p2'};
         this.turn = 0;
@@ -10,69 +9,67 @@ class TicTacToe {
     
     // Methods
     initialiseGame() {
-        for (let i = 0; i < this.spaces.length; i++) {
-            this.spaces[i].addEventListener('click', (e) => {
+        const spaces = document.querySelectorAll('.box')
+        for (let i = 0; i < spaces.length; i++) {
+            spaces[i].addEventListener('click', (e) => {
                 this.drawsXO(i);
             })
         }
     }
-
     drawsXO(num) {
-        this.spaces[num].textContent = this.marker;
-        this.marker == 'X' ? this.marker = 'O' : this.marker = 'X'
+        alert(num);
+        document.querySelector(`#${num}`).textContent = this.marker;
     }
 
     // Method that checks the grid before applying the player marker
     
-    checksGrid(grid) {
-        let remainingSpaces = 9
-        for (let i = 0; i < grid.length; i++) {
-            for (let j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] != 0) {
-                    remainingSpaces--
-                    if (remainingSpaces < 1) {
-                        return 'Draw'
-                    }
-                }
-
-                let vertical = ''
-                if (i == 0) {
-                    for (let q = 0; q < grid.length; q++) {
-                        vertical += grid[q][j]
-                    }
-                }
-                if (key[vertical] != undefined) {
-                    return key[vertical]
-                }
-                
-                let horizontal = ''
-                if (j == 0) {
-                    for (let q = 0; q < grid[i].length; q++) {
-                        horizontal += grid[i][q]
-                    }
-                }
-                if (key[horizontal] != undefined) {
-                    return key[horizontal]
-                }
-
-                let diagonal = ''
-                if (i == 0 && j == 0) {
-                    for (let q = 0; q < grid.length; q++) {
-                        diagonal += grid[i+q][j+q]
-                    }
-                } else if (i == 0 && j == 2) {
-                    for (let q = 0; q < grid.length; q++) {
-                        diagonal += grid[i+q][j-q]
-                    }
-                }
-                if (key[diagonal] != undefined) {
-                    return key[diagonal]
-                }
+checksGrid(grid) {
+let remainingSpaces = 9
+for (let i = 0; i < grid.length; i++) {
+for (let j = 0; j < grid[i].length; j++) {
+    if (grid[i][j] != 0) {
+        remainingSpaces--
+        if (remainingSpaces < 1) {
+            return 'Draw'
+        }
+    }
+        let vertical = ''
+        if (i == 0) {
+            for (let q = 0; q < grid.length; q++) {
+                vertical += grid[q][j]
             }
         }
-        return 'Next turn'
+        if (key[vertical] != undefined) {
+            return key[vertical]
+        }
+        
+        let horizontal = ''
+        if (j == 0) {
+            for (let q = 0; q < grid[i].length; q++) {
+                horizontal += grid[i][q]
+            }
+        }
+        if (key[horizontal] != undefined) {
+            return key[horizontal]
+        }
+
+        let diagonal = ''
+        if (i == 0 && j == 0) {
+            for (let q = 0; q < grid.length; q++) {
+                diagonal += grid[i+q][j+q]
+            }
+        } else if (i == 0 && j == 2) {
+            for (let q = 0; q < grid.length; q++) {
+                diagonal += grid[i+q][j-q]
+            }
+        }
+        if (key[diagonal] != undefined) {
+            return key[diagonal]
+        }
+
     }
 }
+return 'Next turn'
+}
 
-let test = new TicTacToe
-test.initialiseGame()
+}
